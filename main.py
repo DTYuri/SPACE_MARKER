51318f56ac1ac1e1116860adba88e9b339540eb4
Yuri Duarte <yurifdy3@gmail.com>
2024-06-25 01:15:03 -0300

    Adicionando cliques na tela

---
+++ main.py
@@ -0,0 +1,40 @@
+### Yuri Duarte Theisen     RA: 1125371         Vinicius Radaelli da Silva  RA: 1135940
+
+import pygame
+from tkinter import simpledialog
+pygame.init()
+
+tamanho_tela = (800, 600)
+tela = pygame.display.set_mode((tamanho_tela))
+nomeJogo = pygame.display.set_caption("Space Marker")
+fundo = pygame.image.load("img/fundo.jpg")
+#icone = 
+relogio = pygame.time.Clock()
+branco = (255,255,255)
+cinza = (190, 190, 190)
+fonte = pygame.font.SysFont("baskerville ", 20)
+nomeEstrelas = {}
+
+
+while True :
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif evento.type == pygame.MOUSEBUTTONUP:
+            posicao = pygame.mouse.get_pos()
+            caixaPergunta = simpledialog.askstring("Space", "Nome da Estrela")
+            if caixaPergunta is None:
+                caixaPergunta = "Não encontrado" +str(posicao)
+            nomeEstrelas[caixaPergunta]= posicao
+
+    tela.fill(branco)
+    tela.blit (fundo, (0,0))
+    for nome, pos in nomeEstrelas.items():
+        texto = fonte.render(nome + str(pos), True, cinza)
+        tela.blit(texto, pos)
+
+
+    pygame.display.update()
+    relogio.tick(60)
+
